7cc3c3a71ff8349fa5d6ab5f0ebf1d42f5609242
Андрей Жученко <andrey.zhuchenko@nebusfinance.ru>
2025-07-28 23:13:07 +0400

    fix

--- src/notifications.py
+++ src/notifications.py
@@ -11,8 +11,12 @@ async def send_error_notification(code: int, text: str, method: str):
 async def send_vacancy_notification(resume_title: str, vacancy: HhVacancyModel):
     test_text = "❗️ ЕСТЬ ТЕСТОВОЕ\n\n" if vacancy.has_test else ""
     text = (
-        f"{test_text}💡 <u>{vacancy.title}</u>\n<i>{vacancy.employer} "
-        f"({vacancy.area})</i>\n{vacancy.work_format}\n\n<i>{vacancy.description}</i>\n\n{resume_title.lower()}"
+        (
+            f"{test_text}💡 <u>{vacancy.title}</u>\n<i>{vacancy.employer} "
+            f"({vacancy.area})</i>\n{vacancy.work_format}\n\n<i>{vacancy.description}</i>\n\n{resume_title.lower()}"
+        )
+        .replace("<", " ")
+        .replace(">", " ")
     )
     kb = VacanciesInline.vacancy_kb(vacancy=vacancy)
     await bot.send_message(chat_id=config.bot.admin_id, text=text, reply_markup=kb)
