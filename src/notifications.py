35dcfd59c17c7c6b65160047f0450f323420e7a8
Андрей Жученко <andrey.zhuchenko@nebusfinance.ru>
2025-07-29 10:11:24 +0400

    fix

--- src/notifications.py
+++ src/notifications.py
@@ -11,12 +11,11 @@ async def send_error_notification(code: int, text: str, method: str):
 async def send_vacancy_notification(resume_title: str, vacancy: HhVacancyModel):
     test_text = "❗️ ЕСТЬ ТЕСТОВОЕ\n\n" if vacancy.has_test else ""
     text = (
-        (
-            f"{test_text}💡 <u>{vacancy.title}</u>\n<i>{vacancy.employer} "
-            f"({vacancy.area})</i>\n{vacancy.work_format}\n\n<i>{vacancy.description}</i>\n\n{resume_title.lower()}"
-        )
-        .replace("<", " ")
-        .replace(">", " ")
+        f"{test_text}💡 <u>{vacancy.title}</u>\n<i>{vacancy.employer} "
+        f"({vacancy.area})</i>\n{vacancy.work_format}\n\n<i>{vacancy.description}</i>\n\n{resume_title.lower()}"
     )
     kb = VacanciesInline.vacancy_kb(vacancy=vacancy)
-    await bot.send_message(chat_id=config.bot.admin_id, text=text, reply_markup=kb)
+    try:
+        await bot.send_message(chat_id=config.bot.admin_id, text=text, reply_markup=kb)
+    except Exception as e:
+        await send_error_notification(code=e, text=str(e), method="send_vacancy_notification")
